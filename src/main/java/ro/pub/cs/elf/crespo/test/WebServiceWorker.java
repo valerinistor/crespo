51134bc98f5e6445df372c5cc37bcfbb1ec4ba9c
Valeri Nistor <nistor.valeri@gmail.com>
2014-04-12 16:06:47 +0300

    implement parallel ant tasks

--- src/main/java/ro/pub/cs/elf/crespo/test/WebServiceWorker.java
+++ src/main/java/ro/pub/cs/elf/crespo/test/WebServiceWorker.java
@@ -1,20 +1,18 @@
 package ro.pub.cs.elf.crespo.test;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import javax.swing.SwingWorker;
 
-import ro.pub.cs.elf.crespo.dto.UserFile;
+import ro.pub.cs.elf.crespo.app.Crespo;
 import ro.pub.cs.elf.crespo.dto.User;
 import ro.pub.cs.elf.crespo.mediator.Mediator;
 
 public class WebServiceWorker extends SwingWorker<Void, User> {
 
+	private String[] users = {"bugs", "daffy", "sam"};
 	private Mediator mediator;
 	private int DELAY = 2000;
-	private int count = 5;
 
 	public WebServiceWorker(Mediator mediator) {
 		this.mediator = mediator;
@@ -25,22 +23,11 @@ public class WebServiceWorker extends SwingWorker<Void, User> {
 	 */
 	@Override
 	protected Void doInBackground() throws Exception {
-		Thread.sleep(DELAY);
-
-		Random rand = new Random();
-		for (int i = 0; i < count; i++) {
-			User user = new User("user_" + i);
-			List<UserFile> userFiles = new ArrayList<UserFile>();
-
-			for (int j = 0; j < rand.nextInt(15); j++) {
-				UserFile file = new UserFile(user + "_file_" + j);
-				file.setOwner(user);
-				userFiles.add(file);
+		for (String user : users) {
+			if (!mediator.getMe().toString().toLowerCase().contains(user)) {
+				publish(Crespo.loadUser(user));
+				Thread.sleep(DELAY);
 			}
-			user.setSharedFiles(userFiles);
-
-			publish(user);
-			Thread.sleep(DELAY);
 		}
 		return null;
 	}
